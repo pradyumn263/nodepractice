4a4dd3143301b6a14fa47f6989fc4b3bfccadb62
Pradyumn <pradyumnshkl@users.noreply.github.com>
2020-06-25 19:39:02 +0530

    Section 9 beginning

--- controllers/error.js
+++ controllers/error.js
@@ -1,3 +1,3 @@
 exports.get404 = (req, res, next) => {
-  res.status(404).render("404", { docTitle: "PAGE NOT FOUND", path: '' });
+  res.status(404).render("404", { pageTitle: "PAGE NOT FOUND", path: '' });
 };
