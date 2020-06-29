4c78195ccaaafb927f0fcb9b7c5e910ec66fa176
Pradyumn <pradyumnshkl@users.noreply.github.com>
2020-06-29 23:56:15 +0530

    Installed sequelize

--- models/product.js
+++ models/product.js
@@ -1,4 +1,4 @@
-const dp = require("../util/database");
+const db = require("../util/database");
 const Cart = require("./cart");
 
 module.exports = class Product {
@@ -10,13 +10,20 @@ module.exports = class Product {
 		this.price = price;
 	}
 
-	save() {}
+	save() {
+		return db.execute(
+			"INSERT INTO products (title, price, imageUrl, description) VALUES (?,?,?,?)",
+			[this.title, this.price, this.imageUrl, this.description]
+		);
+	}
 
 	static deleteById() {}
 
 	static fetchAll() {
-		return dp.execute("SELECT * FROM products");
+		return db.execute("SELECT * FROM products");
 	}
 
-	static findById(id) {}
+	static findById(id) {
+		return db.execute("SELECT * FROM products WHERE products.id = ?", [id]);
+	}
 };
