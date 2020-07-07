3bda624a608ad0b72ef1078a3edf53c36088f603
Pradyumn <pradyumnshkl@users.noreply.github.com>
2020-07-08 00:40:53 +0530

    Authentication v1 and Mailing set Up

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,5 +1,24 @@
-const User = require("../models/user");
 const bcrypt = require("bcryptjs");
+const nodemailer = require("nodemailer");
+const xoauth2 = require("xoauth2");
+const sendgridTransport = require("nodemailer-sendgrid-transport");
+
+const User = require("../models/user");
+
+const transporter = nodemailer.createTransport({
+	service: "gmail",
+	auth: {
+		type: "OAuth2",
+		user: "pradysshop@gmail.com",
+		clientId:
+			"495558664491-33bl1kp3qv22ihtk1rofm3rk8cjgm6hj.apps.googleusercontent.com",
+		clientSecret: "I_6Fyf8qbUwXsOYYy_6ye78T",
+		refreshToken:
+			"1//04jbhXNGyaxO5CgYIARAAGAQSNwF-L9IrOP0JqQNH-A006T3yekDuAk4gRuMfhrcd2uEkzUuyYr41AIEDVN5l1C-Mjqnk1l33lDI",
+		accessToken:
+			"ya29.a0AfH6SMCpP_p9FK-xAHESaoZh8EiNWxnmHrxib-Yd-ovhcSurIWe30K6llTeSM3sStPMWQRzQPSMT-Jd6j-lgqZdUagPOVGO01RKjblOHd5WvQn9EK66LNzz-DNQ9g9bH5xzGgTk_u5f7ykrc1lMzg3Yr5gAr5UjgPbY",
+	},
+});
 
 exports.getLogin = (req, res, next) => {
 	let message = req.flash("error");
@@ -92,7 +111,26 @@ exports.postSignup = (req, res, next) => {
 					})
 					.then((result) => {
 						res.redirect("/login");
+						let mailOptions = {
+							from: "Prady's Shop <pradyumnshukla26@gmail.com",
+							to: email.toString(),
+							subjext: "Thank You For Signing Up!",
+							text: "Thank you very much for signing up to my Shop!",
+						};
+						return transporter.sendMail(mailOptions, (err, res) => {
+							if (err) {
+								console.log(err);
+							} else {
+								console.log("Email Sent!");
+							}
+						});
 					});
+				// .then(() => {
+				// 	console.log("Email sent");
+				// })
+				// .catch((err) => {
+				// 	console.log(err);
+				// });
 			}
 		})
 		.catch((err) => {
